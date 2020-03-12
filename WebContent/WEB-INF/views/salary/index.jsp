16e8fcf82108ce5f1865c8569cec55f8df4c9ce2
hiroto <hiiroo.hiiroo1103@gmail.com>
2020-03-12 12:38:58 +0900

    Add Salary index

--- WebContent/WEB-INF/views/salary/index.jsp
+++ WebContent/WEB-INF/views/salary/index.jsp
@@ -1,11 +1,47 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>Insert title here</title>
-    </head>
-    <body>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:if test="${flush != null}">
+            <div id="flush_success">
+                <c:out value="${flush}"></c:out>
+            </div>
+        </c:if>
+        <h2>給料 一覧</h2>
+        <table id="salary_list">
+            <tbody>
+                <tr>
+                    <th class="salary_date">日付</th>
+                    <th class="shop_name">店名</th>
+                    <th class="salary_hour">勤務時間</th>
+                    <th class="salary_wage">時給</th>
+                </tr>
+                <c:forEach var="salary" items="${salaries}" varStatus="status">
+                    <tr class="row${status.count % 2}">
+                        <td class="salary_date"><fmt:formatDate value='${salary.job_date}' pattern='yyyy-MM-dd' /></td>
+                        <td class="shop_name">${salary.shop_name}</td>
+                        <td class="salary_hour">${salary.hour}</td>
+                        <td class="salary_wage">${salary.hourly_wage}</td>
+                    </tr>
+                </c:forEach>
+            </tbody>
+        </table>
 
-    </body>
-</html>
+        <div id="pagination">
+            （全 ${salaries_count} 件）<br />
+            <c:forEach var="i" begin="1" end="${((salaries_count - 1) / 15) + 1}" step="1">
+                <c:choose>
+                    <c:when test="${i == page}">
+                        <c:out value="${i}" />&nbsp;
+                    </c:when>
+                    <c:otherwise>
+                        <a href="<c:url value='/salary/index?page=${i}' />"><c:out value="${i}" /></a>&nbsp;
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+        </div>
+        <p><a href="<c:url value='/salary/new' />">新規バイトの登録</a></p>
+
+    </c:param>
+</c:import>
--- src/controllers/salary/SalaryIndexServlet.java
+++ src/controllers/salary/SalaryIndexServlet.java
@@ -1,19 +1,26 @@
 package controllers.salary;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Salary;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class SalaryIndexServlet
  */
 @WebServlet("/salary/index")
 public class SalaryIndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +29,39 @@ public class SalaryIndexServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(Exception e) {
+            page = 1;
+        }
+        List<Salary> salaries = em.createNamedQuery("getAllSalaries", Salary.class)
+                                  .setFirstResult(15 * (page - 1))
+                                  .setMaxResults(15)
+                                  .getResultList();
+
+        long salaries_count = (long)em.createNamedQuery("getSalariesCount", Long.class)
+                                     .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("salaries", salaries);
+        request.setAttribute("salaries_count", salaries_count);
+        request.setAttribute("page", page);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/salary/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
